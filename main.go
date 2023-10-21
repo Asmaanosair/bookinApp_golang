9f1150cdda9c6f38bbf09635b46f011b5316fbc5
AsmaaNusair <asmaanosair5@gmail.com>
2023-10-21 16:06:14 +0300

    new

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("helooword")
+	var conferenceName string
+	var conferenceTicket int
+	var remaining int
+	 conferenceName= "Ticket App"
+	 conferenceTicket =80
+	  remaining = 20 
+	fmt.Println("Welcom to our" ,conferenceName, "application ")
+	fmt.Println("confranceTick =" ,conferenceTicket , "and remainig =" , remaining )
+	fmt.Println("Get out tickes from here ")
+    fmt.Printf("Welcom to our %v application\n",conferenceName)
+	fmt.Printf("Welcom to our %v application\n",conferenceName)
+	fmt.Printf("confranceTick = %v  and remainig = %v\n" , conferenceTicket ,remaining)
+	fmt.Printf("Get out tickes from here \n")
+		
+	
+
 }
